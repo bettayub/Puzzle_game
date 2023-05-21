323542b1a7c181f25c6efc4fe7e8390d7700707a
bettayub <bettayub19@gmail.com>
2023-05-21 10:51:18 +0300

    added timeout for wrong selection

--- main.js
+++ main.js
@@ -13,17 +13,27 @@ counter++;
 }else {
     secondSelection = icon.getAttribute("pic");
     counter = 0;
+
     if(firstSelection===secondSelection){
         const correctCards = document.querySelectorAll(
-            ".icon[pic='"+firstSelection+ "']"  );
+            ".icon[pic='" + firstSelection + "']"  );
             correctCards[0].classList.add("checked");
             correctCards[0].classList.remove("clicked");
             correctCards[1].classList.add("checked");
             correctCards[1].classList.remove("clicked");
          }else {
-            const incorrectcards = document.querySelectorAll(".icon.clicked");
+            const incorrectCards = document.querySelectorAll(".icon.clicked");
 
-            
+            incorrectCards[0].classList.add("shake");
+            incorrectCards[1].classList.add("shake");
+
+            setTimeout(() => {
+                incorrectCards[0].classlist.remove("shake");
+                incorrectCards[0].classlist.remove("clicked");
+                incorrectCards[1].classlist.remove("shake");
+                incorrectCards[1].classlist.remove("clicked");
+
+            },800);
          }
            
          
