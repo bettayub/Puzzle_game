1194b8afa25e1d8dbdb643b33eb3118ccb5ccdf9
bettayub <bettayub19@gmail.com>
2023-05-22 11:16:27 +0300

    added font size tostartbutton

--- main.js
+++ main.js
@@ -48,14 +48,73 @@ cards.forEach((icon) => {
       icon.style.order = randomPos;
     });
   })();
-  
-  
-  
-const startButton = document.getElementById('start-button');
 const timerElement = document.getElementById('timer');
 const movesElement = document.getElementById('moves');
 const starRatingElement = document.getElementById('star-rating');
 
+let movesCount = 0;
+let starRating = 5;
+let timerInterval;
+let minutes = 0;
+let seconds = 0;
 
+const startButton = document.getElementById('start-button');
+function startGame() {
+  movesCount = 0;
+  starRating = 5;
+  minutes = 0;
+  seconds = 0;
+
+  // Reset elements
+  movesElement.textContent = `Moves: ${movesCount}`;
+  starRatingElement.textContent = `Star Rating: ${starRating}`;
+  updateTimer();
+
+  // Start the timer
+  timerInterval = setInterval(updateTimer, 1000);
+
+  // Add event listeners to the cards
+  const cards = document.querySelectorAll('.icon');
+  cards.forEach((card) => {
+    card.addEventListener('click', cardClickHandler);
+  });
+
+  // Hide start button
+  startButton.style.display = 'none';
+}
+
+// Function to handle card click
+function cardClickHandler() {
+  // Handle the logic for card click here
+  // ...
   
-  
+  // Increment moves count
+  movesCount++;
+  movesElement.textContent = `Moves: ${movesCount}`;
+
+  // Update star rating based on moves count
+  if (movesCount > 10) {
+    starRating = 4;
+  } else if (movesCount > 15) {
+    starRating = 3;
+  } else if (movesCount > 20) {
+    starRating = 2;
+  } else if (movesCount > 25) {
+    starRating = 1;
+  }
+  starRatingElement.textContent = `Star Rating: ${starRating}`;
+}
+
+// Function to update the timer
+function updateTimer() {
+  seconds++;
+  if (seconds === 60) {
+    seconds = 0;
+    minutes++;
+  }
+
+  timerElement.textContent = `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+}
+
+// Event listener for the start button
+startButton.addEventListener('click', startGame);
